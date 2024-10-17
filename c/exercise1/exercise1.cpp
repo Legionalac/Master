5d49cba809692f719c7a3ef7897f964a977e631c
Nikola Pavicevic <nikolapavicevic95@gmail.com>
2024-10-17 16:11:27 +0200

    Finished exercise1

--- c/exercise1/exercise1.cpp
+++ c/exercise1/exercise1.cpp
@@ -1,41 +1,63 @@
 #include <iostream>
 
 struct Node {
-	Node() {}
-	Node(int e, Node* n) : elem(e), next(n) {}
-	int elem = 0;
-	Node* next = nullptr;
+    Node() {}
+    Node(int e, Node* n) : elem(e), next(n) {}
+    int elem = 0;
+    Node* next = nullptr;
+
+    struct Iterator {
+        Node* current;
+        Iterator(Node* node) : current(node) {}
+
+        int& operator*() {
+            return current->elem;
+        }
+
+        Iterator& operator++() {
+            current = current->next;
+            return *this;
+        }
+
+        bool operator!=(const Iterator& other) const {
+            return current != other.current;
+        }
+    };
+
+    Iterator begin() { return Iterator(next); }
+    Iterator end() { return Iterator(nullptr); }
 };
 
 void add(Node& n, int x) {
-	n.next = new Node(x, n.next);
+    n.next = new Node(x, n.next);
 }
 
-
 int main() {
-	Node head;
-	add(head, 1);
-	add(head, 2);
-	add(head, 3);
-	add(head, 4);
-	add(head, 5);
+    Node head;
+    add(head, 1);
+    add(head, 2);
+    add(head, 3);
+    add(head, 4);
+    add(head, 5);
 
-	for (Node* p = head.next; p != nullptr; p = p->next) {
-		std::cout << p->elem << " ";
-	}
-	std::cout << std::endl;
+    for (Node* p = head.next; p != nullptr; p = p->next) {
+        std::cout << p->elem << " ";
+    }
+    std::cout << std::endl;
 
-	for (int x : head) {
-		std::cout << x << " ";
-	}
-	std::cout << std::endl;
-	for (int& x : head) {
-		x = 7;
-	}
-	for (const int& x : head) {
-		std::cout << x << " ";
-	}
-	std::cout << std::endl;
+    for (int x : head) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
 
-	return 0;
+    for (int& x : head) {
+        x = 7;
+    }
+
+    for (const int& x : head) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
---
+++ c/exercise1/exercise2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+constexpr int fibo(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    int a = 0;
+    int b = 1;
+    int result = 0;
+
+    for (int i = 2; i <= n; ++i) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
+}
+
+int main() {
+    static_assert(fibo(7) == 13, "Fibonacci(7) should be 13");
+    //Fixed this line so that it is not 34, so that the code runs
+
+    constexpr int k = fibo(9);
+    std::cout << k << std::endl;
+
+    //constexpr int l = fibo(300); // 300. Fibonacci number is too big for int
+
+    return 0;
+}
