0a7de5b0664c05bdca9c38d9a309a6018dc54b86
Nikola Pavicevic <nikola.pavicevic@rt-rk.com>
2024-10-21 09:04:05 +0200

    c: Started exercise 3

---
+++ c/exercise3/MyBigInt.hpp
@@ -0,0 +1,121 @@
+#ifndef _MY_BIG_INT_
+#define _MY_BIG_INT_
+
+#include <algorithm>
+#include <cstdint>
+#include <array>
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <ostream>
+#include <string>
+
+struct MyBigInt {
+
+private:
+    static constexpr int size{70};
+    std::shared_ptr<int_least8_t[]> digits;
+
+    void fillWithZero() {
+        std::fill_n(digits.get(), size, 0);
+    }
+public:
+     explicit MyBigInt(unsigned long long number) {
+        digits = std::shared_ptr<int_least8_t[]>(new int_least8_t[size](), std::default_delete<int_least8_t[]>());
+        int i = 0;
+        while (number) {
+            digits[i++] = number % 10;
+            number /= 10;
+        }
+    }
+
+    explicit MyBigInt(const char* number) {
+        int size = std::strlen(number) - 1;
+        int i = 0;
+        while (size >= 0) {
+            digits[i] = number[size] - '0';
+            i++;
+            size--;
+        }
+    }
+    MyBigInt(const MyBigInt& other) {
+        digits = other.digits;
+    }
+    ~ MyBigInt(){
+        digits.reset();
+    }
+
+    MyBigInt operator+(const MyBigInt& other){
+        MyBigInt result {int(0)} ;
+        int carry = 0;
+        for (int i = 0; i < size; i++) {
+            int sum = digits[i] + other.digits[i] + carry;
+            result.digits[i] = sum % 10;
+            carry = sum / 10;
+        }
+
+        return result;
+    }
+
+    friend MyBigInt operator+(const int first , MyBigInt const& second){
+        // Sum only first digit and then sum carry
+        MyBigInt result {int(0)} ;
+        int carry = 0;
+        int sum = second.digits[0] + first;
+        result.digits[0] = sum % 10;
+        carry = sum / 10;
+        int i = 1;
+
+        while (carry != 0) {
+            sum = second.digits[i] + carry;
+            result.digits[i] = sum % 10;
+            carry = sum / 10;
+            i++;
+        }
+        return result;
+    }
+
+    friend MyBigInt operator+( MyBigInt const& first , const int second){
+        // Sum only first digit and then sum carry
+        MyBigInt result = first ;
+        int carry = 0;
+        int sum = first.digits[0] + second;
+        result.digits[0] = sum % 10;
+        carry = sum / 10;
+        int i = 1;
+
+        while (carry != 0) {
+            sum = first.digits[i] + carry;
+            result.digits[i] = sum % 10;
+            carry = sum / 10;
+            i++;
+        }
+        return result;
+    }
+    friend bool operator!=(MyBigInt const& first, MyBigInt const& second){
+        for (int i = 0; i < size; i++) {
+            if(first.digits[i] != second.digits[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+    friend std::ostream& operator<<(std::ostream& os, MyBigInt const& data)
+    {
+        std::string output = "";
+        for(int i = size - 1; i >= 0; i--)
+            if(data.digits[i] != 0)
+                output += std::to_string(data.digits[i]);
+        return os << output;
+    };
+
+
+};
+
+MyBigInt operator""_mbi(const char* x){
+    MyBigInt result{x};
+    std::cout<< "******************** " << result << std::endl;
+    return result;
+}
+
+#endif
---
+++ c/exercise3/exercises1.cpp
@@ -0,0 +1,41 @@
+﻿#include "MyBigInt.hpp"
+
+#include <iostream>
+
+// Here write the following function:
+// MyBigInt fibo(int n);
+
+
+MyBigInt fibo(int n){
+    MyBigInt a{int(0)};
+    MyBigInt b{int(1)};
+    MyBigInt result{int(0)};
+
+    for (int i = 2; i <= n; ++i) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
+
+}
+
+
+int main()
+{
+	MyBigInt k{ 7 };
+	k = fibo(9);
+	k = k + 5;
+	std::cout << k << std::endl;
+	k = 7 + fibo(11);
+	std::cout << k << std::endl;
+	k = 13_mbi;
+	std::cout << k << std::endl;
+	 MyBigInt l = fibo(300);
+	 if (l != 222232244629420445529739893461909967206666939096499764990979600_mbi) {
+	 	std::cout << "Error!" << std::endl;
+	 	return 1;
+	 }
+	std::cout << l << std::endl;
+	return 0;
+}
